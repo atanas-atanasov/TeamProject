3e2eceb976838a3f48e93e6d8f897231c43c7064
atanas-atanasov <atanasov.atanas25@gmail.com>
2015-09-26 10:51:18 +0300

    start

--- TeamProject-Exam/TeamProject-Exam/Properties/AssemblyInfo.cs
+++ TeamProject-Exam/TeamProject-Exam/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Proba")]
+[assembly: AssemblyTitle("TeamProject_Exam")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Proba")]
+[assembly: AssemblyProduct("TeamProject_Exam")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("97b0f842-9061-4ada-9519-4275ffab7444")]
+[assembly: Guid("8d05cf9a-33c9-4a0d-b2a8-d13126547f27")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
